763d0f9ffa459aba00c520c88784668f25605fbd
GaKain <ck66661254@gmail.com>
2023-04-13 16:31:32 +0900

    no message

--- MidnightTrain2/Assets/Scripts/Environment/Object/ObjectMeshStudy.cs
+++ MidnightTrain2/Assets/Scripts/Environment/Object/ObjectMeshStudy.cs
@@ -11,15 +11,19 @@ public class ObjectMeshStudy : MonoBehaviour
     [SerializeField]
     private Vector3[] _originVertexs, _nowVertexs; //오브젝트의 초기 메쉬 정점과 현재 정점의 위치를 저장할 배열
 
+    [SerializeField]
+    private Vector3[] _verticsVelocitys; //각 정점의 이동 속도
+
     void Start()
     {
         _meshFilter = GetComponent<MeshFilter>();   // 메쉬 필터를 받아옴
         _mesh = _meshFilter.mesh;                   // 메쉬 필터의 메쉬를 받아옴
         _originVertexs = _mesh.vertices;            // 오브젝트의 점정을 초기 정점 배열에 대입
         _nowVertexs = _originVertexs;               // 오브젝트의 점정을 현재 정점 배열에 대입
+        _verticsVelocitys = new Vector3[_originVertexs.Length]; //각 정점의 이동 속도 리셋
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         
     }
